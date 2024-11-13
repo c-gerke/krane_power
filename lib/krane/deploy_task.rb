2fd505da7b1879d59a770e0ce01a46a3299f27a8
Colton Gerke <colton.gerke@powerhrg.com>
2024-11-13 10:53:42 -0600

    Re-order deployment sequence

--- lib/krane/deploy_task.rb
+++ lib/krane/deploy_task.rb
@@ -70,15 +70,13 @@ module Krane
       ).map { |r| [r, default_group] }
 
       after_crs = %w(
-        Deployment
-        Service
         Pod
       ).map { |r| [r, default_group] }
 
       crs = cluster_resource_discoverer.crds.select(&:predeployed?).map { |cr| [cr.kind, { group: cr.group }] }
-      svc = cluster_resource_discoverer.services.select(&:predeployed?).map { |svc| [svc.kind, { group: svc.group }] }
       deployment = cluster_resource_discoverer.deployments.select(&:predeployed?).map { |deployment| [deployment.kind, { group: deployment.group }] }
-      Hash[before_crs + crs + svc + deployment + after_crs]
+      svc = cluster_resource_discoverer.services.select(&:predeployed?).map { |svc| [svc.kind, { group: svc.group }] }
+      Hash[before_crs + crs + deployment + svc + after_crs]
     end
 
     def prune_allowlist
